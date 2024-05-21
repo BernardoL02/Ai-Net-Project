a8ccf2f80a8fe01dcff4945d6d5655b2b063b00b
Bernardo Lopes <bernaslopes0708.bl@gmail.com>
2024-05-21 18:12:16 +0100

    FavIcon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,13 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>CineLiz</title>
+    <title>CineLizz</title>
+    <link rel="icon" href='/resources/img/home.png'>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts AND CSS Fileds -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
 </head>
 
 <body class="font-sans antialiased">
